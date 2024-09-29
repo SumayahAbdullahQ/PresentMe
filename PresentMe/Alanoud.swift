f6c2f6846acbcd159036dfeb48cf72580c55c4b9
SumayahAbdullahQ <sumayahabdullah27@gmail.com>
2024-09-29 15:06:59 +0300

    your file

---
+++ PresentMe/Alanoud.swift
@@ -0,0 +1,18 @@
+//
+//  Alanoud.swift
+//  PresentMe
+//
+//  Created by Sumayah Alqahtani on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+struct Alanoud: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Alanoud()
+}
---
+++ PresentMe/Maha.swift
@@ -0,0 +1,18 @@
+//
+//  Maha.swift
+//  PresentMe
+//
+//  Created by Sumayah Alqahtani on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+struct Maha: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Maha()
+}
