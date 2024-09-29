e957e4250211cad457b2d19434ba4508de2cdd98
Anoud75 <Alanoudsaud75@gmail.com>
2024-09-29 16:24:53 +0300

    more info added

--- PresentMe/Alanoud.swift
+++ PresentMe/Alanoud.swift
@@ -9,12 +9,17 @@ import SwiftUI
 
 struct Alanoud: View {
     var body: some View {
-        VStack()
+        VStack(spacing: 20)
         {
             Image(.alanoud)
                 .resizable()
                 .scaledToFit()
                 .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                .frame(width: 400, height: 400)
+                .cornerRadius(400)
+            Text("Alanoud Alsamil")
+            Text("Age 24")
+            Text("Respiratory Therapist")
                
         }
             
