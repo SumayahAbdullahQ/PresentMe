66c6e460e85458aef97df0ea04c03ce5327c698b
Anoud75 <Alanoudsaud75@gmail.com>
2024-09-29 15:42:26 +0300

    sultan

--- PresentMe/Alanoud.swift
+++ PresentMe/Alanoud.swift
@@ -9,7 +9,15 @@ import SwiftUI
 
 struct Alanoud: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack()
+        {
+            Image(.alanoud)
+                .resizable()
+                .scaledToFit()
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+               
+        }
+            
     }
 }
 
