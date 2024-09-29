0f961cfcdb4b4708f62fcaf279a13e1e5d4ff03c
SumayahAbdullahQ <sumayahabdullah27@gmail.com>
2024-09-29 14:47:50 +0300

    Initial Commit

---
+++ PresentMe/PresentMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PresentMeApp.swift
+//  PresentMe
+//
+//  Created by Sumayah Alqahtani on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct PresentMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
